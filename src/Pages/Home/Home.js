6b43eb886c330c7bd68a56f335b555f5254c27b8
mahmudul1995 <mahmuduls451@gmail.com>
2022-05-26 06:20:04 +0600

    home page feature add

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Footer from '../Shared/Footer';
 import Banner from './Banner';
+import Faq from './Faq';
+import Partners from './Partners';
 import Parts from './Parts';
 import Reviews from './Reviews';
 import Stats from './Stats';
@@ -12,6 +14,8 @@ const Home = () => {
             <Parts></Parts>
             <Stats></Stats>
             <Reviews></Reviews>
+            <Partners></Partners>
+            <Faq></Faq>
             <Footer></Footer>
         </div>
     );
